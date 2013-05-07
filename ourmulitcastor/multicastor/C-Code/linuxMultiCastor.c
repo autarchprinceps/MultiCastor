417b93bf4cffdaa16815f19b4867b52dc746abec
autarch princeps <autarch@outlook.com>
2013-05-07 10:47:36 +0200

    Vollständiger Installer

--- ourmulitcastor/multicastor/C-Code/linuxMultiCastor.c
+++ ourmulitcastor/multicastor/C-Code/linuxMultiCastor.c
@@ -3,6 +3,53 @@
 
 int main (){
 
+    char ch;
+	char *source_file;
+   
+
+
+	int cfileexists(const char * filename){
+		/* try to open file to read */
+		FILE *file;
+		if (file = fopen(filename, "rb")){
+		    fclose(file);
+		    return 1;
+		}
+		return 0;
+	}
+
+	int filecopy(const char * source_file, const char * target_file){
+		if(!cfileexists(target_file)){
+			FILE *source, *target;
+			source = fopen(source_file, "rb");
+			target = fopen(target_file, "wb");
+			if(target==NULL){
+				printf("Can not create new lib file!\n");
+			}else{
+				size_t n, m;
+				unsigned char buff[8192];
+				do {
+					n = fread(buff, 1, sizeof buff, source);
+					if (n) m = fwrite(buff, 1, n, target);
+					else   m = 0;
+				} while ((n > 0) && (n == m));
+				if (m) perror("copy");
+	
+			   printf("File copied successfully.\n");
+			   fclose(target);
+		   }
+		   
+		   fclose(source);
+		   return 1;	
+		}else{
+	
+			printf("libjnetpcap already exists in %s!\n",target_file);
+			return 0;
+		}
+	}
+
+
+
 	FILE *fp;
   	char path[3];
 	int bitVersion = 0;
@@ -18,13 +65,26 @@ int main (){
   	}
 
   	pclose(fp);
-	
+
+	char target_file[]="/usr/lib/libjnetpcap.so";
   	switch(bitVersion){
-		case 32: system("sudo java -Djava.library.path=lib/linux/32 -jar MultiCastor.jar");
+		case 32: 
+			source_file=strdup("lib/jnetpcap-1.3.0.ubuntu32/libjnetpcap.so");
+			if(cfileexists(source_file)){
+				filecopy(source_file,target_file);
+				system("sudo java -jar MultiCastor.jar");
+			}else
+			 	printf("libjnetpcap not found in %s",source_file);
 			 break;
 		
-		case 64:system("sudo java -Djava.library.path=lib/linux/64 -jar MultiCastor.jar");
-			break;
+		case 64:
+			source_file=strdup("lib/jnetpcap-1.3.0.ubuntu64/libjnetpcap.so");
+			if(cfileexists(source_file)){
+				filecopy(source_file,target_file);
+				system("sudo java -jar MultiCastor.jar");
+			}else
+			 	printf("libjnetpcap not found in %s",source_file);
+			 break;
 	
 		default:printf("Could not start MultiCastor.");
 			break;
--- ourmulitcastor/multicastor/C-Code/windowsMultiCastor.c
+++ ourmulitcastor/multicastor/C-Code/windowsMultiCastor.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 #include <tchar.h>
 
@@ -21,12 +23,85 @@ BOOL IsWow64(){
     return bIsWow64;
 }
 
-int main( void ){
+int main()
+{
 
-    if(IsWow64())
-        system("start javaw -Djava.library.path=lib/windows/64 -jar MultiCastor.jar");
-    else
-        system("start javaw -Djava.library.path=lib/windows/32 -jar MultiCastor.jar");
-
+    char ch;
+     char *source_file, *target_file;
+     short int i;
+   FILE *source, *target;
+/*
+ * Check if a file exist using fopen() function
+ * return 1 if the file exist otherwise return 0
+ */
+int cfileexists(const char * filename){
+    /* try to open file to read */
+    FILE *file;
+    if (file = fopen(filename, "rb")){
+        fclose(file);
+        return 1;
+    }
     return 0;
+}
+
+int filecopy(const char * source_file, const char * target_file){
+	if(!cfileexists(target_file)){
+		source = fopen(source_file, "rb");
+		target = fopen(target_file, "wb");
+		
+		size_t n, m;
+		unsigned char buff[8192];
+		do {
+		    n = fread(buff, 1, sizeof buff, source);
+		    if (n) m = fwrite(buff, 1, n, target);
+		    else   m = 0;
+		} while ((n > 0) && (n == m));
+		if (m) perror("copy");
+	
+	   printf("File copied successfully.\n");
+	   fclose(target);
+	   fclose(source);
+	   return 1;	
+	}else{
+	
+		printf("jnetpcap already exists in %s!\n",target_file);
+		return 0;
+	}
+}
+  
+
+   if(IsWow64()){
+      source_file=strdup("lib\\jnetpcap-1.3.0-1.win64\\jnetpcap.dll");
+      //Sysnative ist ein alias für System32 (fancy shit)
+	 target_file=strdup("C:\\Windows\\Sysnative\\jnetpcap.dll");
+   }
+		
+      
+   
+ 
+   if(!cfileexists(source_file) )
+   {
+      printf("jnetpcap.dll not found in %s \n",source_file);
+      getchar();
+      exit(EXIT_FAILURE);
+   }
+   
+	filecopy(source_file,target_file);
+	
+	//32 - bit Version: (wird auch im 64 bit system benötigt)
+	source_file=strdup("lib\\jnetpcap-1.3.0-1.win32\\jnetpcap.dll");
+    target_file=strdup("C:\\Windows\\System32\\jnetpcap.dll");
+    if(!cfileexists(source_file) )
+   {
+      printf("jnetpcap.dll not found in %s \n",source_file);
+      getchar();
+      exit(EXIT_FAILURE);
+   }
+    filecopy(source_file,target_file);
+		
+
+
+    system("start javaw -jar MultiCastor.jar");
+ 	
+   return 0;
 }
